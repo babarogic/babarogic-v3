9a25abc700c0cd994e22a739d58ac29fb389a5dd
Goran Babarogic <gbabarogic@gmail.com>
2020-03-10 23:10:34 +0100

    hotjar update fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,17 +54,7 @@ module.exports = {
       resolve: `gatsby-plugin-hotjar`,
       options: {
         id: 1342169,
-        sv:
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:1342169,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
+        sv: 6
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
